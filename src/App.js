a812e213842aac5d61447a34f8460c91cea624ee
code978 <earningonline2100@gmail.com>
2021-06-22 18:17:41 +0530

    changes in ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import UserData from './components/UserData';
+import home from './components/home';
+import image from './components/image';
+import Header from './components/Header';
+
+const Routing = () => {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={home} />
+        <Route path="/form" component={UserData} />
+        <Route path="/image" component={image} />
+      </Switch>
+    </Router>
+  )
+}
+
+
 
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Routing />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,28 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import UserData from './components/UserData';
-import home from './components/home';
-import image from './components/image';
-import Header from './components/Header';
-
-const Routing = () => {
-  return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={home} />
-        <Route path="/form" component={UserData} />
-        <Route path="/image" component={image} />
-      </Switch>
-    </Router>
-  )
-}
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routing />
+    <App />
   </React.StrictMode>
   , document.getElementById('root'));
